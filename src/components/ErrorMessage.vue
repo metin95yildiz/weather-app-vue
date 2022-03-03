9dc28b7e194b0e4bb5ea4f824dab4509a3af9d70
metin95yildiz <metin_y1995@hotmail.com>
2022-03-04 00:15:16 +0300

    Edited ErrorMessage component for i18n

--- src/components/ErrorMessage.vue
+++ src/components/ErrorMessage.vue
@@ -1,19 +1,28 @@
 <template>
     <transition name="fade" appear>
         <div class="alert alert-danger" v-show="getError">
-            <p>{{ $t("errorMessage")}}</p>
+            <p>{{ t("errorMessage")}}</p>
         </div>
     </transition>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import { useI18n } from "vue-i18n";
 
 export default {
       name: "ErrorMessage",
       computed: {
           ...mapGetters(["getError"])
-      }
+      },
+    setup() {
+    const { t } = useI18n({
+      inheritLocale: true,
+      useScope: 'local'
+        })
+
+        return { t }
+    }
 }
 </script>
 
