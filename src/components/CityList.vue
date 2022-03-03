9ae87eee9689f68c2681d5500cabe01fb88bf26e
metin95yildiz <metin_y1995@hotmail.com>
2022-03-03 19:36:33 +0300

    Created CityList component

---
+++ src/components/CityList.vue
@@ -0,0 +1,29 @@
+<template>
+        <ul class="city-list" v-if="getCities" @after-enter="afterEnter">
+            <City v-for="city in getCities" :city="city" :key="city.name" />
+        </ul>
+</template>
+
+<script>
+import City from "./City.vue"
+import { mapGetters } from "vuex";
+
+export default {
+      name: "CityList",
+      components: {
+          City
+      },
+      computed: {
+          ...mapGetters(["getCities"])
+      },
+      methods: {
+          afterEnter() {
+              this.$store.dispatch("getCityData")
+          }
+      }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
