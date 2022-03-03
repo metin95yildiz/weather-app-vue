ea17f2868095a0184e167d95db718945d6857d28
metin95yildiz <metin_y1995@hotmail.com>
2022-03-03 19:38:25 +0300

    Added i18n dependency

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
+import VueI18n from "vue-i18n";
+import { en, tr, jp, fr, de} from "./locales"
 
-createApp(App).mount('#app')
+const i18n = new VueI18n({
+    locale: "en",
+    messages: {
+        en,
+        de,
+        fr,
+        jp,
+        tr
+    }
+  })
+
+createApp(App).use( i18n, store ).mount('#app')
