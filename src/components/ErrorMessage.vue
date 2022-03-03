3fbdd00ba4b0e30bffe13beb054c783ca6009ca7
metin95yildiz <metin_y1995@hotmail.com>
2022-03-03 19:36:05 +0300

    Created ErrorMessage component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,27 @@
+<template>
+    <transition name="fade" appear>
+        <div class="alert alert-danger" v-show="getError">
+            <p>{{ $t("errorMessage")}}</p>
+        </div>
+    </transition>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+      name: "ErrorMessage",
+      computed: {
+          ...mapGetters(["getError"])
+      }
+}
+</script>
+
+<style lang="scss" scoped>
+.fade-enter, .fade-leave-to {
+    opacity: 0;
+}
+.fade-enter-active, .fade-leave-active {
+    transition: opacity 1s;
+}
+</style>
