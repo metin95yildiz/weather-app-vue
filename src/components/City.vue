a3548254f1f537e863198cdea2446c9c702064af
metin95yildiz <metin_y1995@hotmail.com>
2022-03-04 00:15:50 +0300

    Edited City component

--- src/components/City.vue
+++ src/components/City.vue
@@ -1,17 +1,17 @@
 <template>
     <transition-group name="slide">
-        <li class="city">
-            <div class="city-name">
-                <h2>{{city.name}}</h2>
-                <button class="remove-button" title="Remove city" @click="removeCity(city.name)"></button>
+        <li class="city list-group-item rounded" :key="city.name">
+            <div class="city-name position-relative">
+                <h2 class="fw-bolder">{{city.name}}</h2>
+                <button class="position-absolute border-0 top-0 end-0" title="Remove city" @click="remove(city.name)"></button>
             </div>
-            <div class="current-weather">
+            <div class="d-flex current-weather">
                 <Current :current="city.details.current"
                 :timezone="city.details.timezone"/>
             </div>
-            <div class="weekly-forecast">
+            <div class="d-flex weekly-forecast overflow-auto">
               <Card v-for="(day, index) in city.details.daily" :key="index"
-              :day="day" :isToday="index === 0 ? true : false"/>
+              :day="day" :cityName="city.name" :isToday="index === 0 ? true : false"/>
             </div>
         </li>
     </transition-group>
@@ -19,22 +19,47 @@
 
 <script>
 import Card from "./Card.vue";
-import { mapMutations } from "vuex";
+import Current from "./Current.vue";
+
 export default {
     name: "City",
     props: {
         city: Object
     },
     components: {
-      Card
+      Card,
+      Current
     },
-    computed: {
-      ...mapMutations("removeCity")
+    methods: {
+      remove(value){
+        this.$store.dispatch("removeCityData", {
+          city: value
+        })
+      }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+li.city {
+  background-color: rgb(128, 128, 128, 0.5);
+  .city-name {
+      button{
+            height: 2.5rem;
+            width: 2.5rem;
+            background-color: rgb(0, 0, 0, 0);
+            background-image: url("../assets/remove.svg");
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: 2.5rem;
+            opacity: 0.5;
+            transition: opacity 0.3s;
+            &:hover {
+              opacity: 1;
+            }
+      }
+  }
+}
 .slide-enter, .slide-leave-active {
     opacity: 0;
 }
