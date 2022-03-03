05e4a2432fff35f46c176ce9306bdc79a7c68e02
metin95yildiz <metin_y1995@hotmail.com>
2022-03-03 19:32:54 +0300

    Added inital locales file

---
+++ src/locales/index.js
@@ -0,0 +1,5 @@
+export { default as en } from "./en"
+export { default as fr } from "./fr"
+export { default as de } from "./de"
+export { default as jp } from "./jp"
+export { default as tr } from "./tr"
