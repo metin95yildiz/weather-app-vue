6bc651b34097b2d69275f1b693c6944e93f7c692
metin95yildiz <metin_y1995@hotmail.com>
2022-03-03 19:35:23 +0300

    Created initial Time component

---
+++ src/components/Time.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="time-box">
+        <div class="time">
+            <span className="am-pm">
+            </span>
+        </div>
+        <div class="date">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Time",
+    data() {
+        return {
+            date: new Date()
+        } 
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
