e6528df597b9524f308df0ad16202151b5090bda
metin95yildiz <metin_y1995@hotmail.com>
2022-03-05 00:25:40 +0300

    Edited Card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,43 +1,55 @@
 <template>
-    <div class="card">
+    <div class="card mx-2 my-2 mw-100">
         <div class="card-body">
             <h4 class="card-title">
-                {{isToday ?  $t("todayCity", { city: cityName, day: $t(dayName) }) : $t(dayName) }}
+                {{isToday ?  t("todayCity", { city: cityName, day: t(days[dayName]) }) : t(days[dayName]) }}
             </h4>
-            <div class="img-thumbnail">
+            <div>
                 <img
-                src="https://openweathermap.org/img/wn//{{day.weather[0].icon}}@4x.png`"
+                :src="'https://openweathermap.org/img/wn//'+day.weather[0].icon+'@4x.png'"
                 alt="weather-icon"
-                class="weather-icon"
+                class="img-thumbnail"
                 />
             </div>
             <div class="card-text">
-                <p><span>{{ $t((day.weather[0].main).toLowerCase()) }}</span></p>
-                <p>{{ $t("night", { temp: day.temp.night }) }}</p>
-                <p>{{ $t("day", { temp: day.temp.day }) }}</p>
+                <p class="fw-bolder fs-4">{{ t((day.weather[0].main).toLowerCase()) }}</p>
+                <p class="fw-bolder">{{ t("night", { temp: day.temp.night }) }}</p>
+                <p class="fw-bolder">{{ t("day", { temp: day.temp.day }) }}</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { useI18n } from "vue-i18n";
 
 export default {
       name: "Card",
     props: {
         day: Object,
-        isToday: Boolean
+        isToday: Boolean,
+        cityName: String,
+        dayName: Number
     },
     data() {
         return {
             days: ["sun", "mon", "tues", "wed",
                 "thu", "fri", "sat"],
-            dayName: this.days[new Date(this.day.dt*1000).getDay()]
         }
+    },
+      setup() {
+        const { t } = useI18n({
+        inheritLocale: true,
+        useScope: 'local'
+        })
+
+        return { t }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.card {
+    min-width: 15rem;
+}
 </style>
