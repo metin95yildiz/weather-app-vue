68ba8b4c1d03bcdaa56f576b4d2dbe60b0af0f3a
metin95yildiz <metin_y1995@hotmail.com>
2022-03-04 22:50:38 +0300

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
     <ErrorMessage />
     <CityList />
   </div>
+  <footer>
+    <div class="mx-4 mw-50">
+        <h1>Weather App</h1>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -33,13 +38,31 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #151616;
-  background-image: url("https://source.unsplash.com/random/?nature");
-  background-repeat: no-repeat;
-  background-size: cover;
-  text-align: center;
+* {
+      &::-webkit-scrollbar {
+    width: 1rem;
+    }
+    &::-webkit-scrollbar-track {
+        background: rgb(128, 128, 128, 0.5);
+    }
+    &::-webkit-scrollbar-thumb {
+        background: rgb(233, 233, 233);
+        border-radius: 1rem;
+    }
 }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #151616;
+    background-image: url("https://source.unsplash.com/random/?nature");
+    background-repeat: no-repeat;
+    background-size: cover;
+    text-align: center;
+    footer {
+      display: inline-block;
+      bottom: 0;
+      background-color: rgb(128, 128, 128, 0.5);
+      width: 100%;
+    }
+  }
 </style>
