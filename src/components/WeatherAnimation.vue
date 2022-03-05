b8846281ea7c7a8ef0795dd11b33233cebf36ee4
metin95yildiz <metin_y1995@hotmail.com>
2022-03-05 18:36:54 +0300

    Created WeatherAnimation component

---
+++ src/components/WeatherAnimation.vue
@@ -0,0 +1,27 @@
+<template>
+    <Clear v-if="weather === 'Clear'" />
+    <Rain v-else-if="weather === 'Rain'" />
+    <Snow v-else-if="weather === 'Snow'" />
+    <Clouds v-else-if="weather === 'Clouds'" />
+</template>
+
+<script>
+import Clear from "./weatherConditions/Clear.vue";
+import Rain from "./weatherConditions/Rain.vue";
+import Snow from "./weatherConditions/Snow.vue";
+import Clouds from "./weatherConditions/Clouds.vue";
+
+export default {
+    name: "Header",
+    components: {
+        Clear,
+        Rain,
+        Snow,
+        Clouds
+    },
+    props: {
+        weather: String
+    }
+}
+
+</script>
